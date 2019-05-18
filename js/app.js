9826d55148bc2917e269a45e602a3df7f125432b
AyoMoses <odukoyaayodeji@gmail.com>
2019-05-19 00:30:28 +0100

    environment setup

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
+// KURSORX SCRIPTS
 var kursorx = new kursor({
-    type: 2,
+    type: 2, //type to of 5
     color: 'rgba(58, 134, 255, 1)',
-    el: 'body'
+    el: 'body' //to show on any element target
 })
