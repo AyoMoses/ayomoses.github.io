8fa7427db89d5719c43d08633e7eabdda76c5317
AyoMoses <odukoyaayodeji@gmail.com>
2019-05-19 01:00:22 +0100

    hero development

--- js/app.js
+++ js/app.js
@@ -2,5 +2,5 @@
 var kursorx = new kursor({
     type: 2, //type to of 5
     color: 'rgba(58, 134, 255, 1)',
-    el: 'body' //to show on any element target
+    // el: 'body' //to show on any element target
 })
