43e5e18086538558c12ca08b5fa9983a49f579f1
AyoMoses <odukoyaayodeji@gmail.com>
2019-05-21 23:39:23 +0100

    CSS BUG fix

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 var kursorx = new kursor({
     type: 2, //type to of 5
     color: 'rgba(58, 134, 255, 1)',
-    // el: 'body' //to show on any element target
+    removeDefaultCursor: true
 })
 
 // TOGGLE MENU SCRIPT
