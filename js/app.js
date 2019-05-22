3285ec7470742a4dd7eb2cab81e57206400279f1
AyoMoses <odukoyaayodeji@gmail.com>
2019-05-22 18:18:38 +0100

    page navigation fix

--- js/app.js
+++ js/app.js
@@ -33,13 +33,36 @@ open.addEventListener("click", function(){
 
 // JAVASCRIPT TEST
 
-let navContainer = document.getElementById("site-nav-bar");
-let navItems = navContainer.getElementsByClassName("nav-item");
+// let navContainer = document.getElementById("site-nav-bar");
+// let navItems = navContainer.getElementsByClassName("nav-item");
 
-for (var i = 0; i < nav-DataTransferItem.length; i++) {
-    nav-navItems[i].addEventListener("click", function() {
-        let current = document.getElementsByClassName("active");
-        current[0].className = current[0].className.replace("active", "");
-        this.className += " active";
+// for (var i = 0; i < nav-DataTransferItem.length; i++) {
+//     nav-navItems[i].addEventListener("click", function() {
+//         let current = document.getElementsByClassName("active");
+//         current[0].className = current[0].className.replace("active", "");
+//         this.className += " active";
+//     });
+// }
+
+let mainNavLinks = document.querySelectorAll("nav ul li a");
+let mainSections = document.querySelectorAll("main section");
+
+let lastId;
+let cur = [];
+
+window.addEventListener("scroll", event => {
+    let fromTop = window.scrollY;
+
+    mainNavLinks.forEach(link => {
+        let section = document.querySelector(link.hash);
+
+        if (
+            section.offsetTop <= fromTop &&
+            section.offsetTop + section.offsetHeight > fromTop
+        ) {
+            link.classList.add("current");
+        } else {
+            link.classList.remove("current");
+        }
     });
-}
+});
